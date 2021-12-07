c2a34befa2728bbf9687049be56eb1d549ff25f6
Lun-Dev <lunthedev@gmail.com>
2021-12-07 17:31:57 +0800

    Added: Basic structure

--- index.js
+++
@@ -1,4 +0,0 @@
-import moment from 'moment';
-
-console.log("Hello from javascript");
-console.log(moment().calendar());
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
-    },
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
